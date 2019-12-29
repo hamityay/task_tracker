7899f1fb4d618f9627f090bafd432c085c582b9e
hamityay <hamityay@hotmail.com>
2019-12-29 19:44:24 +0300

    Add to_do_card container.

---
+++ src/containers/to_do_card_container.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ToDoCard from '../components/to_do_card'
+
+function ToDoCardContainer(props){
+  function renderCards(){
+    return(
+      props.cards.map(card => {
+        return <ToDoCard key={card.id} handleClickList={props.handleClickList} addList={props.addList} card={card} />
+      })
+    )
+  }
+
+  return(
+    <div>
+      {renderCards()}
+    </div>
+  )
+}
+
+export default ToDoCardContainer
